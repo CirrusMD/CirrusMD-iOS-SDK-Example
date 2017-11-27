0edc491d7535d7df325964389f3c5bb82d0460ab
Sean Dougherty <seancdougherty@gmail.com>
2017-11-27 14:21:26 -0700

    Move framework location

--- Example/Pods/Target Support Files/CirrusMDSDK/CirrusMDSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CirrusMDSDK : NSObject
-@end
-@implementation PodsDummy_CirrusMDSDK
-@end
--- Example/Pods/Target Support Files/Pods-CirrusMDSDK_Example/Pods-CirrusMDSDK_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CirrusMDSDK_Example/Pods-CirrusMDSDK_Example-frameworks.sh
@@ -102,10 +102,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/../../CirrusMDSDK/CirrusMDSDK.framework"
+  install_framework "${PODS_ROOT}/../../CirrusMDSDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/../../CirrusMDSDK/CirrusMDSDK.framework"
+  install_framework "${PODS_ROOT}/../../CirrusMDSDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
