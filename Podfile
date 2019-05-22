3d14078391a0ccda6589f4c23c0ffe1b19b989be
David Nix <davidnix81@gmail.com>
2019-05-22 11:43:01 -0600

    add cocoapods specific target

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CirrusMD/podspecs.git'
+use_frameworks!
+project 'CirrusMDSDK-Example.xcodeproj'
+
+platform :ios, '10.0'
+target 'CirrusMDSDK-Pods' do
+  pod 'CirrusMDSDK'
+  pod 'JWT'
+end
+
---
+++ Pods/CirrusMDSDK/CirrusMDSDK.framework/Headers/LoadingView.h
@@ -0,0 +1,13 @@
+//
+//  LoadingView.h
+//  CirrusMD
+//
+//  Created by David Nix on 9/15/15.
+//  Copyright (c) 2015 CirrusMD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingView : UIView
+
+@end
