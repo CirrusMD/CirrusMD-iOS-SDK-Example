bc57201b5ccf677bf289905d61f5642078bf3edb
Taylor Case <taylor-case@cirrusmd.com>
2020-01-22 14:26:08 -0700

    Add Objective-C example app.

---
+++ CirrusMDSDK-Pods-ObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  CirrusMDSDK-Pods-ObjC
+//
+//  Created by Taylor Case on 1/22/20.
+//  Copyright © 2020 steam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
